709a08885d5fa7f471a75406fce42dd53558e413
Joel Rehra <joelrehra@gmx.de>
2025-09-14 04:46:25 +0200

    Change demo subscriber topic to door

--- common/src/main/java/art/rehra/mineqtt/blocks/RedstoneSubscriberBlock.java
+++ common/src/main/java/art/rehra/mineqtt/blocks/RedstoneSubscriberBlock.java
@@ -23,7 +23,7 @@ public class RedstoneSubscriberBlock extends MineQTTBlock {
 
     protected void onPlace(BlockState state, Level level, BlockPos pos, BlockState oldState, boolean movedByPiston) {
         MineQTT.LOGGER.debug("Placing MineQTT block at " + pos);
-         subscribeToMqttTopic(MineQTTConfig.getTopicPath("tuer"));
+         subscribeToMqttTopic(MineQTTConfig.getTopicPath("door"));
         level.scheduleTick(pos, this, 1);
     }
 
